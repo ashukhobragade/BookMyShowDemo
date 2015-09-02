3601dfcbaef88043e50865363945b7e3453eb81c
ASHISH KHOBRAGADE <ashishkhobragade@outlook.com>
2015-09-02 21:41:11 +0530

    Initial Commit

---
+++ BookMyShowDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BookMyShowDemo
+//
+//  Created by AshU on 9/2/15.
+//  Copyright (c) 2015 Ashish Khobragade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
